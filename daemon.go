e01b9a1a0dedd2877df799d3f8cdf1745907a684
dancheg97 <dancheg97@fmnx.su>
2022-12-04 16:06:42 +0300

    some stuff

--- daemon.go
+++ daemon.go
@@ -1,16 +1,43 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 	"time"
+
+	"github.com/otiai10/copy"
 )
+
+const backupTime = 60 * 60 * 24
 
 func main() {
 	copyTo := os.Getenv(`COPY_TO`)
 	fmt.Println(`will be copying to: `, copyTo)
+
+	i := 0
 	for {
+		if i%backupTime == 0 {
+			err := copy.Copy(`.`, copyTo)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+		}
+
+		var buf bytes.Buffer
+		cmd := exec.Command(`git`, `clone`)
+		cmd.Stdout = &buf
+
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		if strings.Contains()
 		time.Sleep(time.Second)
-		
 	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module gitea.dancheg97.ru/dancheg97/composer
 
 go 1.19
+
+require github.com/otiai10/copy v1.9.0
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
