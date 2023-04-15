25931a14e8b4d54ea12f15a3ac9c46c4d1f9f4b4
dancheg97 <dancheg97@fmnx.su>
2023-04-15 09:56:05 +0300

    fixed accesses

--- certs.sh
+++ certs.sh
@@ -3,8 +3,10 @@ go install github.com/go-acme/lego/v4/cmd/lego@latest
 sudo lego --email="help@fmnx.ru" --domains="fmnx.ru" --http run
 sudo lego --email="help@fmnx.ru" --domains="ci.fmnx.ru" --http run
 sudo lego --email="help@fmnx.ru" --domains="up.fmnx.ru" --http run
+sudo lego --email="help@fmnx.ru" --domains="pkg.fmnx.ru" --http run
 sudo lego --email="help@fmnx.ru" --domains="logs.fmnx.ru" --http run
 sudo lego --email="help@fmnx.ru" --domains="docs.fmnx.ru" --http run
 sudo lego --email="help@fmnx.ru" --domains="tree.fmnx.ru" --http run
+sudo lego --email="help@fmnx.ru" --domains="cloud.fmnx.ru" --http run
 sudo chown -R dancheg97:dancheg97 .lego
 docker compose up
