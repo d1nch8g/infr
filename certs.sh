5654c88d1ed782ed5ec3ed160e0ce2874c0e6fae
dancheg97 <dancheg97@fmnx.su>
2023-04-15 05:35:40 +0300

    removed trash components

--- certs.sh
+++ certs.sh
@@ -1,12 +1,10 @@
 docker compose down
 go install github.com/go-acme/lego/v4/cmd/lego@latest
-sudo lego --email="dangdancheg@gmail.com" --domains="dancheg97.ru" --http run
-sudo lego --email="dangdancheg@gmail.com" --domains="gitea.dancheg97.ru" --http run
-sudo lego --email="dangdancheg@gmail.com" --domains="drone.dancheg97.ru" --http run
-sudo lego --email="dangdancheg@gmail.com" --domains="pocketbase.dancheg97.ru" --http run
-sudo lego --email="dangdancheg@gmail.com" --domains="pacman.dancheg97.ru" --http run
-sudo lego --email="dangdancheg@gmail.com" --domains="kuma.dancheg97.ru" --http run
-sudo lego --email="dangdancheg@gmail.com" --domains="dozzle.dancheg97.ru" --http run
-sudo lego --email="dangdancheg@gmail.com" --domains="fmtree.dancheg97.ru" --http run
+sudo lego --email="help@fmnx.ru" --domains="fmnx.ru" --http run
+sudo lego --email="help@fmnx.ru" --domains="ci.fmnx.ru" --http run
+sudo lego --email="help@fmnx.ru" --domains="up.fmnx.ru" --http run
+sudo lego --email="help@fmnx.ru" --domains="logs.fmnx.ru" --http run
+sudo lego --email="help@fmnx.ru" --domains="docs.fmnx.ru" --http run
+sudo lego --email="help@fmnx.ru" --domains="tree.fmnx.ru" --http run
 sudo chown -R dancheg97:dancheg97 .lego
 docker compose up
