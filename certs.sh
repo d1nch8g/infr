4ca891b7ed9a2a8e4bd4b3bfa30f02ec9b7a28f5
dancheg97 <dancheg97@fmnx.su>
2023-04-24 09:39:59 +0300

    update for publishing articles

--- certs.sh
+++ certs.sh
@@ -7,6 +7,6 @@ sudo lego --email="help@fmnx.ru" --domains="pkg.fmnx.ru" --http run
 sudo lego --email="help@fmnx.ru" --domains="logs.fmnx.ru" --http run
 sudo lego --email="help@fmnx.ru" --domains="docs.fmnx.ru" --http run
 sudo lego --email="help@fmnx.ru" --domains="tree.fmnx.ru" --http run
-sudo lego --email="help@fmnx.ru" --domains="cloud.fmnx.ru" --http run
+sudo lego --email="help@fmnx.ru" --domains="articles.fmnx.ru" --http run
 sudo chown -R dancheg97:dancheg97 .lego
 docker compose up
