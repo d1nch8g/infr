538c44568d7e3f968525200b405ad4bad9c8fca7
dancheg97 <dancheg97@fmnx.su>
2023-04-28 11:16:02 +0300

    updated gitea settings

--- certs.sh
+++ certs.sh
@@ -3,7 +3,7 @@ go install github.com/go-acme/lego/v4/cmd/lego@latest
 sudo lego --email="help@fmnx.io" --domains="fmnx.io" --http run
 sudo lego --email="help@fmnx.io" --domains="ci.fmnx.io" --http run
 sudo lego --email="help@fmnx.io" --domains="up.fmnx.io" --http run
-sudo lego --email="help@fmnx.io" --domains="pkg.fmnx.io" --http run
+sudo lego --email="help@fmnx.io" --domains="repo.fmnx.io" --http run
 sudo lego --email="help@fmnx.io" --domains="logs.fmnx.io" --http run
 sudo lego --email="help@fmnx.io" --domains="docs.fmnx.io" --http run
 sudo lego --email="help@fmnx.io" --domains="tree.fmnx.ru" --http run
