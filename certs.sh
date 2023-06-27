d75c3d6c433e5bce1af76839f354926d552c2946
dancheg97 <dancheg97@fmnx.su>
2023-06-27 15:23:25 +0300

    fixed navbar

--- certs.sh
+++ certs.sh
@@ -1,8 +1,5 @@
 docker compose down
 go install github.com/go-acme/lego/v4/cmd/lego@latest
 sudo lego --email="dancheg@fmnx.su" --domains="fmnx.su" --http run
-sudo lego --email="dancheg@fmnx.su" --domains="ci.fmnx.su" --http run
-sudo lego --email="dancheg@fmnx.su" --domains="up.fmnx.su" --http run
-sudo lego --email="dancheg@fmnx.su" --domains="pk.fmnx.su" --http run
 sudo chown -R dancheg97:dancheg97 .lego
 docker compose up
